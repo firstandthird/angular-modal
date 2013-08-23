5866561b4938ee39872dcdbae061376aeef41c28
Greg Allen <me@jga.me>
2013-08-23 07:59:46 -0700

    initial commit

---
+++ dist/angular-modal.full.js
@@ -0,0 +1,9 @@
+/*!
+ * angular-modal - A simple modal directive
+ * v0.1.0
+ * http://github.com/firstandthird/angular-modal/
+ * copyright First + Third 2013
+ * MIT License
+*/
+
+//angular-modal.js
---
+++ dist/angular-modal.js
@@ -0,0 +1,8 @@
+/*!
+ * angular-modal - A simple modal directive
+ * v0.1.0
+ * http://github.com/firstandthird/angular-modal/
+ * copyright First + Third 2013
+ * MIT License
+*/
+//angular-modal.js
