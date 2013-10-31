cc2b9f0f8a0e1706445c4a03e29999d5adcb6058
Greg Allen <me@jga.me>
2013-10-31 09:19:33 -0700

    renamed attributes to overlay, added overlayClose to scope

--- example/app.js
+++ example/app.js
@@ -2,4 +2,19 @@ angular.module('example-app', ['modal']);
 
 var MainController = function($scope) {
   $scope.world = 'World';
+  $scope.name = '';
+
+  $scope.$watch('name', function(val) {
+    console.log('name changed', val);
+  });
+
+  $scope.overlayClose2 = function() {
+    if ($scope.name == '') {
+      alert('name must be filled out');
+      return;
+    }
+
+    $scope.overlayClose();
+  };
+
 };
